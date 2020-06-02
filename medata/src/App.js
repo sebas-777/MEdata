3ba7284829b3d457319d8418948052d69fb26f58
sebas-777 <53665624+sebas-777@users.noreply.github.com>
2020-06-02 23:34:34 +0200

    1.0.1

--- medata/src/App.js
+++ medata/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './components/Buscador';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  
+  state = {
+    termino:''
+  }  
+
+  consultarApi = () =>{
+  
+  const termino = this.state.termino
+  const url = `http://medata.gov.co/api/action/datastore/search.json?resource_id=d778f6a8-c002-4443-9ec4-be71f6382751&q=${termino}`;
+
+ // console.log(url);
+ fetch(url)
+    .then(respuesta =>respuesta.json())
+    .then(resultado =>console.log(resultado) )
+  
+  }
+  
+  datosBusqueda =(termino)=>{
+   this.setState({
+     termino
+   },() => {
+     this.consultarApi();
+   })
+  }
+  
+  render(){
+    return(
+      <div className = "app container">
+        <div className ="jumbotron">
+        <p className ="lead text-center" >Techos presupuestales presupuesto participativo</p>
+        <Buscador
+          datosBusqueda={this.datosBusqueda}
+        />
+        </div>
+        {this.state.termino}
+      </div>
+    );
+  }
 }
 
 export default App;
